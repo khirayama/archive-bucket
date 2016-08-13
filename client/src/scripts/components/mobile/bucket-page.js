f74d89d034ce115bb32e5519b5e4d70439203f02
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-08-13 17:02:35 +0900

    Create map and user class

--- client/src/scripts/components/mobile/bucket-page.js
+++ client/src/scripts/components/mobile/bucket-page.js
@@ -4,16 +4,92 @@ import React, { Component } from 'react';
 const propTypes = {
 };
 
-function createMap(element) {
-  return new google.maps.Map(element, {
-    scrollwheel: true,
-    zoom: 17,
-    disableDefaultUI: true,
-  });
-}
+class Map {
+  constructor(mapElement, inputElement) {
+    this._mapElement = mapElement;
+    this._inputElement = inputElement;
 
-function createSearchBox(element) {
-  return new google.maps.places.SearchBox(element);
+    this._map = new google.maps.Map(this._mapElement, {
+      scrollwheel: true,
+      zoom: 17,
+      disableDefaultUI: true,
+    });
+    this._user = new User(this._map);
+    this._markers = [];
+
+    this._service = new google.maps.places.PlacesService(this._map);
+
+    this._getCurrentPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      this._user.setPosition(pos);
+      this._user.showPosition();
+      this._map.setCenter(pos);
+    });
+    this._setEventListeners();
+  }
+  _getCurrentPosition(callback) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      callback(position);
+    });
+  }
+  _setEventListeners() {
+    this._inputElement.addEventListener('keydown', (event) => {
+      const keyCode = event.keyCode;
+
+      if (keyCode === 13) {
+        const pos = this._user.getPosition();
+        this._service.textSearch({
+          query: event.target.value,
+          location: new google.maps.LatLng(pos.lat, pos.lng),
+          radius: 300,
+        }, (places, status) => {
+          if (places.length) {
+            this._inputElement.blur();
+            this._clearMarker();
+            this._showPlaces(places);
+          }
+        });
+      }
+    });
+  }
+  _showPlaces(places) {
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      this._addMarker(new google.maps.Marker({
+        map: this._map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    this._map.fitBounds(bounds);
+  }
+  _addMarker(marker) {
+    this._markers.push(marker);
+  }
+  _clearMarker() {
+    this._markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    this._markers = [];
+  }
 }
 
 class User {
@@ -44,73 +120,27 @@ class User {
       radius: 5,
     });
   }
+  getPosition() {
+    return this._position;
+  }
   setPosition(position) {
-    this._circle.setCenter(position);
-    this._center.setCenter(position);
+    this._position = position;
+  }
+  showPosition() {
+    this._circle.setCenter(this._position);
+    this._center.setCenter(this._position);
   }
 }
 
 export default class BucketPage extends Component {
   componentDidMount() {
-    this._map = createMap(this.refs.map);
-    this._searchBox = createSearchBox(this.refs.searchbox);
-    this._user = new User(this._map);
-    this._markers = [];
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      const pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-      this._user.setPosition(pos);
-      this._map.setCenter(pos);
-    });
-
-    this._map.addListener('bounds_changed', () => {
-      this._searchBox.setBounds(this._map.getBounds());
-    });
-
-    this._searchBox.addListener('places_changed', () => {
-      const places = this._searchBox.getPlaces();
-
-      // clear markers
-      this._markers.forEach((marker) => {
-        this._marker.setMap(null);
-      });
-      this._markers = [];
-
-      const bounds = new google.maps.LatLngBounds();
-      places.forEach((place) => {
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        // Create a marker for each place.
-        this._markers.push(new google.maps.Marker({
-          map: this._map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-
-        if (place.geometry.viewport) {
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      this._map.fitBounds(bounds);
-    });
+    new Map(this.refs.map, this.refs.input);
   }
   render() {
     return (
       <section className="page bucket-page">
         <section className="page-content">
-          <input className="bucket-search-box" type="text" ref="searchbox" />
+          <input autoFocus className="bucket-search-box" type="text" ref="input" />
           <section className="bucket-map" ref="map" />
         </section>
       </section>
